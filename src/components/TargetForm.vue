6fbb1c0409be4c9376eaa9ef0019847db638d5bb
nishimunea <nishimunea@gmail.com>
2019-06-02 10:08:18 +0900

    Squashed 'ui/' changes from 9917df2..3de9709
    3de9709 Merge pull request #1 from recruit-tech/feature/multi-target-registration
    c737863 Add multiple target registration
    
    git-subtree-dir: ui
    git-subtree-split: 3de97091cae19334caafae7e1d163dd9c0834b7e

--- src/components/TargetForm.vue
+++ src/components/TargetForm.vue
@@ -4,12 +4,19 @@
       <div class="col">
         <div class="d-flex justify-content-end">
           <div class="mr-auto flex-fill pr-3">
-            <input
-              type="text"
-              class="form-control"
-              v-model="target"
-              :placeholder="$t('home.target-form.ip-address-or-host-name')"
-            />
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="target"
+                :placeholder="$t('home.target-form.ip-address-or-host-name')"
+              />
+              <div class="input-group-append div-multi-host-registration">
+                <span class="input-group-text" data-toggle="modal" data-target="#modal-multi-host-registration">
+                  <font-awesome-icon icon="file-upload"></font-awesome-icon>
+                </span>
+              </div>
+            </div>
           </div>
           <div>
             <button class="btn btn-primary">{{ $t('home.target-form.register-target') }}</button>
@@ -18,10 +25,13 @@
         <small class="form-text text-danger">{{ errorMessage }}</small>
       </div>
     </div>
+    <modal-multi-host-registration :add-target-api="addTargetApi"></modal-multi-host-registration>
   </form>
 </template>
 
 <script>
+import ModalMultiHostRegistration from './ModalMultiHostRegistration.vue';
+
 export default {
   name: 'TargetForm',
   props: {
@@ -34,6 +44,9 @@ export default {
       required: true
     }
   },
+  components: {
+    ModalMultiHostRegistration
+  },
   data() {
     return {
       target: '',
@@ -41,45 +54,69 @@ export default {
     };
   },
   methods: {
-    addTarget: async function addTarget() {
-      try {
-        if (this.target.length === 0) {
-          this.errorMessage = this.$i18n.t('home.target-form.status.target-is-empty');
-          return;
-        }
-        const res = await this.auditApiClient.post('/scan/', { target: this.target });
-        switch (res.status) {
-          case 200:
-          case 304:
-            this.target = '';
-            this.errorMessage = '';
-            window.eventBus.$emit('SCAN_REGISTERED', res.data.uuid);
-            break;
-          case 400: {
-            const message = JSON.stringify(res.data.message);
-            if (message.indexOf('target-is-not-fqdn-or-ipv4') >= 0) {
-              this.errorMessage = this.$i18n.t('home.target-form.status.target-is-not-fqdn-or-ipv4');
-            } else if (message.indexOf('Audit has been submitted or approved') >= 0) {
-              this.errorMessage = this.$i18n.t('home.target-form.status.audit-submitted');
-            } else if (message.indexOf('target-is-private-ip') >= 0) {
-              this.errorMessage = this.$i18n.t('home.target-form.status.target-is-private-ip');
-            } else if (message.indexOf('could-not-resolve-target-fqdn') >= 0) {
-              this.errorMessage = this.$i18n.t('home.target-form.status.could-not-resolve-target-fqdn');
-            } else {
-              this.errorMessage = message;
-            }
-            break;
+    addTargetApi: async function addTargetApi(target) {
+      const res = await this.auditApiClient.post('/scan/', { target });
+      switch (res.status) {
+        case 200:
+        case 304:
+          return res.data.uuid;
+        case 400: {
+          const message = JSON.stringify(res.data.message);
+          if (message.indexOf('target-is-not-fqdn-or-ipv4') >= 0) {
+            throw new Error(this.$i18n.t('home.target-form.status.target-is-not-fqdn-or-ipv4'));
+          } else if (message.indexOf('Audit has been submitted or approved') >= 0) {
+            throw new Error(this.$i18n.t('home.target-form.status.audit-submitted'));
+          } else if (message.indexOf('target-is-private-ip') >= 0) {
+            throw new Error(this.$i18n.t('home.target-form.status.target-is-private-ip'));
+          } else if (message.indexOf('could-not-resolve-target-fqdn') >= 0) {
+            throw new Error(this.$i18n.t('home.target-form.status.could-not-resolve-target-fqdn'));
+          } else {
+            throw new Error(message);
           }
-          case 401:
-            this.errorMessage = this.$i18n.t('home.target-form.status.invalid-token');
-            break;
-          default:
-            this.errorMessage = this.$i18n.t('home.target-form.status.unknown-error');
         }
+        case 401:
+          throw new Error(this.$i18n.t('home.target-form.status.invalid-token'));
+        default:
+          throw new Error(this.$i18n.t('home.target-form.status.unknown-error'));
+      }
+    },
+    addTarget: async function addTarget() {
+      if (this.target.length === 0) {
+        this.errorMessage = this.$i18n.t('home.target-form.status.target-is-empty');
+        return;
+      }
+      try {
+        const uuid = await this.addTargetApi(this.target);
+        window.eventBus.$emit('SCAN_REGISTERED', uuid);
+        this.target = '';
+        this.errorMessage = '';
       } catch (e) {
-        this.errorMessage = this.$i18n.t('home.target-form.status.unknown-error');
+        this.errorMessage = e.message;
       }
     }
   }
 };
 </script>
+<style scoped>
+.input-group > .div-multi-host-registration {
+  margin-left: -2.5rem;
+  z-index: 2;
+  border: 0px solid rgba(0, 0, 0, 0) !important;
+}
+.input-group > .div-multi-host-registration > .input-group-text {
+  border-width: 0;
+  background-color: rgba(255, 255, 255, 0);
+  opacity: 0.5;
+  transition: 0.5s;
+}
+.input-group > .div-multi-host-registration > .input-group-text:hover {
+  cursor: pointer;
+  opacity: 1;
+  transition: 0.5s;
+}
+.input-group > .custom-select:not(:last-child),
+.input-group > .form-control:not(:last-child) {
+  border-top-right-radius: 0.25rem;
+  border-bottom-right-radius: 0.25rem;
+}
+</style>
